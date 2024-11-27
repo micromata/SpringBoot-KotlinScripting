6438095251e580f62af0c6e5c4b544565dd79857
Kai Reinhard <K.Reinhard@micromata.de>
2024-11-27 01:09:36 +0100

    WIP

--- application/src/main/kotlin/de/micromata/kotlinscripting/DemoApplication.kt
+++ application/src/main/kotlin/de/micromata/kotlinscripting/DemoApplication.kt
@@ -1,17 +1,19 @@
 package de.micromata.kotlinscripting
 
-import Foo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication {
 
-fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<DemoApplication>(*args)
 
-    val scriptExecutor = ScriptExecutor()
-    val result = scriptExecutor.executeScript()
-    println("Script result: $result")
-    Foo().bar()
+            val scriptExecutor = ScriptExecutor()
+            val result = scriptExecutor.executeScript()
+            println("Script result: $result")
+        }
+    }
 }
